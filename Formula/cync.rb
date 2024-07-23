edc9da93cb0f4f10ea63c9cfcf55830f35fd9bba
Adi <subramanianaditya42@gmail.com>
2024-07-23 19:15:18 +0200

    Change version to 0.1.3

--- Formula/cync.rb
+++ Formula/cync.rb
@@ -1,9 +1,9 @@
 class Cync < Formula
   desc "CLI for private file management"
   homepage "https://github.com/JuiceDrinker/cync"
-  url "https://github.com/JuiceDrinker/cync/releases/download/v1.0.0/cync-1.0.0.tar.gz"
+  url "https://github.com/JuiceDrinker/cync/releases/download/v0.1.3/cync-0.1.3.tar.gz"
   sha256 "eb0bb9e52c8df9adda68f6920e477f4edf41dbcaf4a54a56adc03f19c6d3b39e"
-  version "1.0"
+  version "0.1.3"
 
   def install
     bin.install "cync"
